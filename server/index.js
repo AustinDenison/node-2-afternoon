a7cb676db479caf3f0cbe6e40cdfa8c692d4929f
AustinDenison <austindenison1998@gmail.com>
2019-06-24 14:42:43 -0600

    done

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const messagesCtrl = require('./controllers/messages_controller')
+
+app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
+
+app.post('/api/messages', messagesCtrl.create)
+
+app.get('/api/messages', messagesCtrl.read)
+
+app.put('/api/messages/:id', messagesCtrl.update)
+
+app.delete('/api/messages/:id', messagesCtrl.delete)
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`)
+})
